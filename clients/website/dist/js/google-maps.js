31717d47d81b5a789359cbbbed44081419dbc192
Luke Stoward <luke.stoward@students.plymouth.ac.uk>
2017-03-18 15:04:31 +0000

    Created google map section
    Using google maps api I have created a section for displaying the zoos
    dynamically on the map, along with providing the ability to get
    directions.

---
+++ clients/website/dist/js/google-maps.js
@@ -0,0 +1,129 @@
+function initMap() {
+
+    $.get('./zoos.json', (result) => {
+
+      //Configure destination select list with zoos
+      initDestinationSelectList(result);
+
+        var mapOptions = {
+            zoom: 6,
+            center: {
+                lat: 53.775537,
+                lng: -4.006032
+            }
+        };
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        //Configure directions handler
+        new AutocompleteDirectionsHandler(map);
+
+        var infoWindows = [];
+
+        var markers = result.map((zoo, i) => {
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: zoo.lat,
+                    lng: zoo.long
+                },
+                title: zoo.name,
+                animation: google.maps.Animation.DROP,
+                map: map
+            });
+
+            var contentString =
+                '<div id="content"><b>' + zoo.name + '</b>' +
+                '<div id="bodyContent">' +
+                '<a href="' + zoo.link + '">' + zoo.link + '</a> ' +
+                '</div></div>';
+
+            //Create window and add to array
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            infoWindows.push(infowindow);
+
+            marker.addListener('click', function() {
+                //First close all windows
+                infoWindows.map((w, i) => {
+                    w.close()
+                });
+                infowindow.open(map, marker);
+            });
+            return;
+        });
+    });
+}
+
+var AutocompleteDirectionsHandler = function(map) {
+    this.map = map;
+    this.originPlaceId = null;
+    this.destination = null;
+    this.travelMode = "DRIVING";
+
+    var originInput = document.getElementById('origin-input');
+    var destinationInput = document.getElementById('destination-select');
+
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
+    this.directionsDisplay.setMap(map);
+
+    var originAutocomplete = new google.maps.places.Autocomplete(
+        originInput, { placeIdOnly: true });
+
+    //Handle origin selection
+    this.setupPlaceChangedListener(originAutocomplete);
+
+    //Handle zoo selection change
+    destinationInput.addEventListener("change", () => {
+      this.destination = destinationInput.value;
+      this.route();
+    })
+}
+
+AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete) {
+    var me = this;
+    autocomplete.bindTo('bounds', this.map);
+
+    autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (!place.place_id) {
+            window.alert("Please select an option from the dropdown list.");
+            return;
+        }
+        me.originPlaceId = place.place_id;
+        me.route();
+    });
+
+};
+
+AutocompleteDirectionsHandler.prototype.route = function() {
+
+    if (!this.originPlaceId || !this.destination) {
+        return;
+    }
+
+    var me = this;
+
+    //Object containing the input terms
+    var directionsRequest = {
+      origin: { 'placeId': this.originPlaceId },
+      destination: this.destination,
+      travelMode: this.travelMode
+    }
+
+    this.directionsService.route(directionsRequest, function(response, status) {
+        if (status === 'OK') {
+            me.directionsDisplay.setDirections(response);
+        } else {
+            //window.alert('Directions request failed due to ' + status);
+        }
+    });
+};
+
+var initDestinationSelectList = function(items){
+
+    items.map((opt, index) => {
+      $("#destination-select").append(new Option(opt.name, opt.postcode));
+    });
+}
--- clients/website/src/index.js
+++ clients/website/src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { ZooSection } from './components/zoo-section.jsx';
 import { AnimalSection } from './components/animal-section.jsx';
+import { DirectionsSection } from './components/directions-section.jsx';
 
 window.React = React;
 
@@ -16,4 +17,9 @@ render(
 render(
     <AnimalSection />,
     document.getElementById('animals-section')
+)
+
+render(
+    <DirectionsSection />,
+    document.getElementById('directions-section')
 )
